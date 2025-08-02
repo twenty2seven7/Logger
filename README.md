0647acb9bb20356f07902fce07db39c5b219b524
twentyseven <vilesov_s@mail.ru>
2025-08-02 19:45:41 +0500

    create logger

---
+++ README.md
@@ -0,0 +1,57 @@
+# Logger
+
+## ✨ Example Usage
+
+```cpp
+void test_logger() {
+    LOG_SIMPLE("test LOG_SIMPLE ", 1);
+    LOG_INPUT("test LOG_INPUT ", 2);
+    LOG_RESULT("test LOG_RESULT ", 3);
+    LOG_INFO("test LOG_INFO ", 4);
+    LOG_WARNING("test LOG_WARNING ", 5);
+    LOG_ERROR("test LOG_ERROR ", 6);
+}
+```
+
+This code will produce the following output:
+
+![example result](docs/example.png)
+
+---
+
+## 📄 Simple Output
+
+```cpp
+Logger::instance().log(Logger::INFO, "test ", "output");
+```
+
+---
+
+## 📁 File Output
+
+To redirect log output of specific levels to a file:
+
+```cpp
+std::ofstream file("log.txt");
+Logger::instance().set_output(Logger::WARNING, file);
+Logger::instance().set_output(Logger::ERROR, file);
+```
+
+After this, all `WARNING` and `ERROR` messages will be written to `log.txt`.
+
+---
+
+## ⚙️ Output Optimization
+
+To speed up terminal output, call this at the beginning of the program:
+
+```cpp
+Logger::instance().init_stdio();
+```
+
+However, it is usually more convenient to use the predefined macros:
+
+```
+LOG_SIMPLE, LOG_INPUT, LOG_RESULT,
+LOG_INFO, LOG_WARNING, LOG_ERROR
+```
---
+++ logger.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <ctime>
+#include <iomanip>
+#include <mutex>
+
+
+
+class Logger {
+
+    public:
+        enum LogType{
+            SIMPLE,
+            INPUT,
+            RESULT,
+            INFO,
+            WARNING,
+            ERROR,
+            COUNT
+        };
+
+    private:
+        std::mutex write_mutex;
+        std::array<std::ostream*, LogType::COUNT> outputs {
+            &std::cout, // SIMPLE
+            &std::cout, // INPUT
+            &std::cout, // RESULT
+            &std::cout, // INFO
+            &std::cout, // WARNING
+            &std::cout  // ERROR
+        };
+
+        inline std::string color_wrapper(const std::string &text, const std::string &color_code, std::ostream &stream) {
+            if (&stream == &std::cout || &stream == &std::cerr) {
+                return color_code + text + "\033[0m";
+            }
+            return text;
+        }
+
+        Logger() = default;
+        ~Logger() = default;
+        Logger(const Logger&) = delete;
+        Logger& operator=(const Logger&) = delete;
+        Logger(Logger&&) = delete;
+        Logger& operator=(Logger&&) = delete;
+
+        void write(const std::string &text, const LogType log_type = LogType::INFO) {
+            std::lock_guard<std::mutex> lock(write_mutex);
+            std::ostream& out = *outputs.at(static_cast<std::size_t>(log_type));
+            std::time_t now = std::time(nullptr);
+            std::ostringstream current_time;
+            current_time << std::put_time(std::localtime(&now), "%F %T: ");
+            std::string log_level, color;
+            switch (log_type) {
+                case SIMPLE:    out << text << std::endl; return;
+                case INPUT:     log_level = " [INPUT]:   "; color = "\033[34m"; break;
+                case RESULT:    log_level = " [RESULT]:  "; color = "\033[36m"; break;
+                case INFO:      log_level = " [INFO]:    "; color = "\033[32m"; break;
+                case WARNING:   log_level = " [WARNING]: "; color = "\033[33m"; break;
+                case ERROR:     log_level = " [ERROR]:   "; color = "\033[31m"; break;
+                default:        log_level = " [UNKNOWN]: "; color = "";         break;
+            }
+            std::string preffix = color_wrapper(current_time.str() + log_level, color, out);
+            out << preffix << text << std::flush << std::endl;
+        }
+
+    public:
+        static Logger& instance() {
+            static Logger instance;
+            return instance;
+        }
+
+        void  init_stdio() {
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
+        }
+
+        void set_output(LogType log_type, std::ostream &stream) {
+            outputs.at(static_cast<std::size_t>(log_type)) = &stream;
+        }
+
+        template<typename... Args>
+        void log(LogType log_type, Args &&... args) {
+            std::ostringstream oss;
+            (oss << ... << args);
+            write(oss.str(), log_type);
+        }
+};
+
+
+#define LOG_SIMPLE(...)   Logger::instance().log(Logger::SIMPLE, __VA_ARGS__)
+#define LOG_INPUT(...)     Logger::instance().log(Logger::INPUT, __VA_ARGS__)
+#define LOG_RESULT(...)   Logger::instance().log(Logger::RESULT, __VA_ARGS__)
+#define LOG_INFO(...)     Logger::instance().log(Logger::INFO, __VA_ARGS__)
+#define LOG_WARNING(...)  Logger::instance().log(Logger::WARNING, "\033[33m\033[4m", __FILE__, ":", std::to_string(__LINE__), "\033[0m::", __func__, ": ", __VA_ARGS__)
+#define LOG_ERROR(...)    Logger::instance().log(Logger::ERROR, "\033[31m\033[4m", __FILE__, ":", std::to_string(__LINE__), "\033[0m::", __func__, ": ", __VA_ARGS__)
